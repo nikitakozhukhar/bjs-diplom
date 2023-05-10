46eae1be4998e63d904902f6e77f7f572b5a8aee
NikitaKx9 <nikitakx9@gmail.com>
2023-05-10 16:49:33 +0300

    login/register logout profile rates done

--- public/homePage.js
+++ public/homePage.js
@@ -1,5 +1,57 @@
 let logOut = new LogoutButton();
 
-logOut.action = ApiConnector.logout(location.reload())
+logOut.action = e => {
+	ApiConnector.logout(() => {
+		if (logOut.logoutClick) {
+			location.reload();
+		}
+	})
+}
 
-/*Создайте объект класса LogoutButton. В свойство action запишите функцию, которая будет вызывать запрос деавторизации (logout). В колбек запроса добавьте проверку: если запрос выполнился успешно, то обновите страницу (с помощью location.reload();).*/
+ApiConnector.current(data => {
+	if (data.success) {
+		ProfileWidget.showProfile(data.data);
+	}
+});
+
+
+let ratestBoard = new RatesBoard();
+
+ApiConnector.getStocks(data => {
+	ratestBoard.clearTable();
+	ratestBoard.fillTable(data.data);
+})
+
+let timerId = setInterval(() => {
+	ApiConnector.getStocks(data => {
+		if (data.success) {
+			ratestBoard.clearTable();
+			ratestBoard.fillTable(data.data);
+		}
+
+	})
+}, 60000);
+
+let moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback(data => console.log(data))
+
+// console.log(moneyManager)
+/*Создайте объект типа MoneyManager+
+Реализуйте пополнение баланса:
+Запишите в свойство addMoneyCallback функцию, которая будет выполнять запрос.
+Внутри функции выполните запрос на пополнение баланса (addMoney).
+Используйте аргумент функции свойства addMoneyCallback для передачи данных data в запрос.
+После выполнения запроса выполните проверку успешности запроса.
+В случае успешного запроса отобразите в профиле новые данные о пользователе из данных ответа от сервера (showProfile).
+Также выведите сообщение об успехе или ошибку (причину неудачного действия) пополнении баланса в окне отображения сообщения (setMessage).
+Реализуйте конвертирование валюты:
+Запишите в свойство conversionMoneyCallback функцию, которая будет выполнять запрос.
+Внутри функции выполните запрос на конвертацию баланса (convertMoney)
+Используйте аргумент функции свойства conversionMoneyCallback для передачи данных в запрос.
+Повторите пункты 2.4-2.7
+Реализуйте перевод валюты:
+Запишите в свойство sendMoneyCallback функцию, которая будет выполнять запрос.
+Внутри функции выполните запрос на перевод валюты (transferMoney).
+Используйте аргумент функции свойства sendMoneyCallback для передачи данных в запрос.
+Повторите пункты 2.4-2.7 */
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,14 +2,23 @@
 
 let userForm = new UserForm();
 
+
 userForm.loginFormCallback = data => ApiConnector.login(data, response => {
-	if (response) {
-		location.reload();
+	if (response.success) {
+		console.log(response)
+		document.location.reload();
+	} else {
+		console.error(response.error)
 	}
 });
 
-userForm.registerFormCallback = data => ApiConnector.register(data, response => {
-	if (response) {
-		location.reload();
-	}
-});
+userForm.registerFormCallback = data => ApiConnector.register(data, response => console.log(response));
+// 	{
+// 	if (response.success) {
+// 		console.log(response)
+// 		location.reload();
+// 	}
+// 	else {
+// 		console.error(response.error)
+// 	}
+// });
